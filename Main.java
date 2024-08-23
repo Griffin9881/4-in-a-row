e89acbf1a4bd178b3e20429c76e8cad7698b035e
Griffin <lamb.griffin@icloud.com>
2024-08-23 16:55:31 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    private static int[][] board = {{0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}};
+    public static void main(String[] args) {
+    
+    }
+}
